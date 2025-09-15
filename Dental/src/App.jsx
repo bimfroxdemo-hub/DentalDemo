e998b27778df22863d1a5ebf8749c969cb8981ca
Ankit maurya <amaurya64566@gmail.com>
2025-09-15 16:32:37 +0530

    adding

--- Dental/src/App.jsx
+++ Dental/src/App.jsx
@@ -2,7 +2,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-// import NotFound from "./pages/NotFound";
 
 // Create a QueryClient instance
 const queryClient = new QueryClient();
@@ -12,8 +11,6 @@ const App = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-        {/* Add all custom routes above the catch-all "*" route */}
-        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </BrowserRouter>
   </QueryClientProvider>
